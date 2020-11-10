18da9c8ece5735d94c5ac3e4ed96fde65c8b8971
Lilian VERLHAC <lilian.verlhac@epitech.eu>
2020-11-10 17:18:51 +0100

    Fix Argmanager (#8)
    * [FIX] Argument module, using <$> Functor operator for better comprehension
    
    * [ADD] option -h and --help in parseargs to show help

--- src/Arguments.hs
+++ src/Arguments.hs
@@ -1,25 +1,29 @@
 module Arguments
     ( parseArgs,
+      SourceCode (..),
+      IsInterractive (..),
     ) where
 
 import Control.Exception (throw, catch, IOException)
-import Control.Monad (foldM)
 
-import Exception (HExceptions (ArgumentException))
+import Exception (HExceptions (ArgumentException, SendHelp))
 
 type FileContent = String
-data Argument = Computable FileContent
-              | Interpretable FileContent
-              | NoArgument deriving (Show)
+newtype IsInterractive = IsInterractive Bool deriving (Show)
+data SourceCode = SourceCode IsInterractive [FileContent] deriving (Show)
 
-parseArgs :: [String] -> IO Argument
-parseArgs [] = throw $ ArgumentException "hal should be used with at least one argument."
-parseArgs s  = catch (foldM argInterpreter NoArgument s) (\e -> throw $ ArgumentException $ show (e::IOException))
+interactive :: IsInterractive
+interactive = IsInterractive True
 
-argInterpreter :: Argument -> String -> IO Argument
-argInterpreter NoArgument         "-i" = return $ Interpretable ""
-argInterpreter (Computable fc)    "-i" = return $ Interpretable fc
-argInterpreter (Interpretable _)  "-i" = throw $ ArgumentException "Invalid argument, please use \"-h\""
-argInterpreter NoArgument         s    = readFile s >>= (return . Computable)
-argInterpreter (Computable fc)    s    = readFile s >>= (return . Computable . (++ fc))
-argInterpreter (Interpretable fc) s    = readFile s >>= (return . Interpretable . (++ fc))
+computed :: IsInterractive
+computed = IsInterractive False
+
+parseArgs :: [String] -> IO SourceCode
+parseArgs []           = throw $ ArgumentException "hal should be used with at least one argument."
+parseArgs ("-h":_)     = throw SendHelp
+parseArgs ("--help":_) = throw SendHelp
+parseArgs s            = catch (parseArgs' $ reverse s) (\e -> throw $ ArgumentException $ show (e::IOException))
+
+parseArgs' :: [String] -> IO SourceCode
+parseArgs' ("-i":xs) = SourceCode interactive <$> mapM readFile xs
+parseArgs' s         = SourceCode computed    <$> mapM readFile s
