8688e4da66607cd632e1e3836a2ca09d978d09b7
Lilian VERLHAC <lilian.verlhac@epitech.eu>
2020-11-21 23:26:10 +0100

    [ADD] Main

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ import Exception (exceptionHandler)
 import Arguments (parseArgs, SourceCode (..), IsInterractive (..))
 import Lexer (lexer)
 import Parser (parse)
+import Evaluator (evaluate, Value)
 
 main :: IO ()
 main = handle exceptionHandler $ getArgs >>= (halgo . parseArgs)
@@ -16,4 +17,7 @@ main = handle exceptionHandler $ getArgs >>= (halgo . parseArgs)
 halgo :: SourceCode -> IO ()
 halgo SOS                                    = print SOS
 halgo (SourceCode (IsInterractive True ) fc) = mapM (fmap lexer . readFile) fc >>= print
-halgo (SourceCode (IsInterractive False) fc) = print "computable"
+halgo (SourceCode (IsInterractive False) fc) = mapM (fmap (evaluate . parse . lexer) . readFile) fc >>= (toto . concat)
+
+toto :: [Value] -> IO ()
+toto = foldr ((>>) . print) (return ())
